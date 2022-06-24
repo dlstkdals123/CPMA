86ce5abf2e30e354b5ae0e64cce0f4c4b0381303
dlstkdals123 <84954087+dlstkdals123@users.noreply.github.com>
2022-06-24 16:35:55 +0900

    Add files via upload

---
+++ ch02/projects/05/ch02p05.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int x;
+
+	printf("Enter value for x : ");
+	scanf("%d", &x);
+
+	printf("Result : %d", 3 * x * x * x * x * x + 2 * x * x * x * x - 5 * x * x * x - x * x + 7 * x - 6);
+
+	return 0;
+}
