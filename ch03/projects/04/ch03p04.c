9f4495ec1ab49d8c87a76366e68f85da936cbfdf
dlstkdals123 <84954087+dlstkdals123@users.noreply.github.com>
2022-06-24 19:58:22 +0900

    Add files via upload

---
+++ ch03/projects/04/ch03p04.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int phone_num1, phone_num2, phone_num3;
+
+	printf("Enter phone number [(xxx) xxx-xxxx] : ");
+	scanf(" (%d ) %d -%d", &phone_num1, &phone_num2, &phone_num3);
+
+	printf("You entered %d.%d.%d", phone_num1, phone_num2, phone_num3);
+
+	return 0;
+}
