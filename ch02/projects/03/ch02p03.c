d23d7bcb38c0fed8a0126507bceeb700e066754c
dlstkdals123 <84954087+dlstkdals123@users.noreply.github.com>
2022-06-24 16:30:10 +0900

    Update ch02p03.c

--- ch02/projects/03/ch02p03.c
+++ ch02/projects/03/ch02p03.c
@@ -7,7 +7,7 @@ int main(void)
 	printf("Enter radius : ");
 	scanf("%d", &r);
 
-	printf("Sphere volume : %.2f", (4.0f / 3.0f) * 3.14f * 10 * 10 * 10);
+	printf("Sphere volume : %.2f\n", (4.0f / 3.0f) * 3.14f * 10 * 10 * 10);
 
 	return 0;
 }
