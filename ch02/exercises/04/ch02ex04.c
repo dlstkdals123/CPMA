9bf03946c85150a69b43f065514822ff4d691169
dlstkdals123 <84954087+dlstkdals123@users.noreply.github.com>
2022-06-24 15:51:29 +0900

    Add files via upload

---
+++ ch02/exercises/04/ch02ex04.c
@@ -0,0 +1,14 @@
+// Computes the dimensional weight of a 12'' x 10'' x 8'' box
+
+#include <stdio.h>
+
+int main(void)
+{
+	int i, j, k;
+	float x, y;
+
+	printf("i : %d\nj : %d\nk : %d\n", i, j, k);
+	printf("x : %f\ny : %f\n", x, y);
+
+	return 0;
+}
