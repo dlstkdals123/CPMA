68832229c9834e238fab2d756c657fa41bf871d3
dlstkdals123 <84954087+dlstkdals123@users.noreply.github.com>
2022-06-24 16:38:12 +0900

    Add files via upload

---
+++ ch02/projects/06/ch02p06.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int x;
+
+	printf("Enter value for x : ");
+	scanf("%d", &x);
+
+	printf("Result : %d", (((((3 * x + 2) * x - 5) * x - 1) * x + 7) * x - 6));
+
+	return 0;
+}
