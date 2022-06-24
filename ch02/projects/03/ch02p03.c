7149772d23dcf5ab249713ef948129f027af6a79
dlstkdals123 <84954087+dlstkdals123@users.noreply.github.com>
2022-06-24 16:28:12 +0900

    Add files via upload

---
+++ ch02/projects/03/ch02p03.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int r;
+
+	printf("Enter radius : ");
+	scanf("%d", &r);
+
+	printf("Sphere volume : %.2f", (4.0f / 3.0f) * 3.14f * 10 * 10 * 10);
+
+	return 0;
+}
