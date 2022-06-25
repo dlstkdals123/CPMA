421d0aaced6c84eb5b135867e91814b01d17e666
Sangmin In <84954087+dlstkdals123@users.noreply.github.com>
2022-06-25 21:21:42 +0900

    Update ch05p02.c

--- ch05/projects/02/ch05p02.c
+++ ch05/projects/02/ch05p02.c
@@ -10,7 +10,7 @@ int main(void)
 	printf("Equivalent 12-hour time : ");
 	if (hours % 12 == 0) printf("12:%.2d", minutes);
 	else printf("%d:%.2d", hours % 12, minutes);
-	hours < 12 || hours == 24 ? printf(" AM\n") : printf(" PM\n");
+	hours < 12 ? printf(" AM\n") : printf(" PM\n");
 
 	return 0;
 }
