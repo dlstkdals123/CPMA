fb1cf42515686b7bd4c241e9e7647381817375a1
dlstkdals123 <84954087+dlstkdals123@users.noreply.github.com>
2022-06-25 12:27:21 +0900

    Update ch04ex11.c

--- ch04/exercises/11/ch04ex11.c
+++ ch04/exercises/11/ch04ex11.c
@@ -4,18 +4,21 @@ int main(void)
 {
 	int i, j, k;
 
-    i = 1;
-    printf("%d ", i++ - 1);
-    printf("%d\n", i);
-    i = 10; j = 5;
-    printf("%d ", i++ - ++j);
-    printf("%d %d\n", i, j);
-    i = 7; j = 8;
-    printf("%d ", i++ - --j);
-    printf("%d %d\n", i, j);
-    i = 3; j = 4; k = 5;
-    printf("%d ", i++ - j++ + --k);
-    printf("%d %d %d\n", i, j, k);
+	i = 1; // (a)
+	printf("%d ", i++ - 1);
+	printf("%d\n", i);
+	
+	i = 10; j = 5; // (b)
+	printf("%d ", i++ - ++j);
+	printf("%d %d\n", i, j);
+	
+	i = 7; j = 8; // (c)
+	printf("%d ", i++ - --j);
+	printf("%d %d\n", i, j);
+	
+	i = 3; j = 4; k = 5; // (d)
+	printf("%d ", i++ - j++ + --k);
+	printf("%d %d %d\n", i, j, k);
     
 	return 0;
 }
