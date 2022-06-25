0a55c3ff3161b2e4ee4957bba8a55e1992c16110
Sangmin In <84954087+dlstkdals123@users.noreply.github.com>
2022-06-25 21:34:40 +0900

    Update broker.c

--- ch05/myworks/broker.c
+++ ch05/myworks/broker.c
@@ -12,15 +12,15 @@ int main(void)
 	if (value < 2500.00f)
 		commission = 30.00f + .017f * value;
 	else if (value < 6250.00f)
-		commission = 56 + .0066 * value;
+		commission = 56.00f + .0066f * value;
 	else if (value < 20000.00f)
-		commission = 76 + 0.0034 * value;
+		commission = 76.00f + 0.0034f * value;
 	else if (value < 50000.00f)
-		commission = 100 + 0.0022 * value;
+		commission = 100.00f + 0.0022f * value;
 	else if (value < 500000.00f)
-		commission = 155 + 0.0011 * value;
+		commission = 155.00f + 0.0011f * value;
 	else
-		commission = 255 + 0.0009 * value;
+		commission = 255.00f + 0.0009f * value;
 
 	if (commission < 39.00f)
 		commission = 39.00f;
