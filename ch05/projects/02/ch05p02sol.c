3cbcc06ff04481ec330173c942e2c44d464242c3
Sangmin In <84954087+dlstkdals123@users.noreply.github.com>
2022-06-25 21:02:46 +0900

    Update ch05p02sol.c

--- ch05/projects/02/ch05p02sol.c
+++ ch05/projects/02/ch05p02sol.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
 
-int main(void) {
-
+int main(void)
+{
     int hour, minute;
 
     printf("Enter a 24-hour time: ");
     scanf("%d :%d", &hour, &minute);
 
-    if (hour < 11)
-        printf("Equivalent 12-hour time: %d:%d AM\n", 
-                hour == 0 ? 12 : hour, minute);
-    else 
-        printf("Equivalent 12-hour time: %d:%d PM\n", 
-               hour == 12 ? 12 : hour - 12, minute);
+    int ampm_hour = hour >= 12 ? hour % 12 : hour;
+    printf("Equivalent 12 hour time: %d:%d %s", ampm_hour, minute, hour < 12 ? "AM" : "PM");
 
     return 0;
 }
