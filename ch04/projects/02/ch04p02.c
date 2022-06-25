e590e43ff1456ab1df2812851ddd63a5e50275b2
dlstkdals123 <84954087+dlstkdals123@users.noreply.github.com>
2022-06-25 13:56:21 +0900

    Update ch04p02.c

--- ch04/projects/02/ch04p02.c
+++ ch04/projects/02/ch04p02.c
@@ -4,7 +4,7 @@ int main(void)
 {
 	int n;
 
-	printf("Enter a two-digit number : ");
+	printf("Enter a three-digit number : ");
 	scanf("%d", &n);
 
 	printf("The reversal is %d%d%d", n % 10, n % 100 / 10, n / 100);
