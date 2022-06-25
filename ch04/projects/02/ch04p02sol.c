9e7255425184ab33ba2c3534b1e58a83bf582100
dlstkdals123 <84954087+dlstkdals123@users.noreply.github.com>
2022-06-25 14:03:24 +0900

    Update ch04p02sol.c

--- ch04/projects/02/ch04p02sol.c
+++ ch04/projects/02/ch04p02sol.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
-int main(void) {
+int main(void)
+{
+  int n;
 
-    int n = 0;
+  printf("Enter a three-digit number: ");
+  scanf("%d", &n);
+  printf("The reversal is: %d%d%d\n", n % 10, (n / 10) % 10, n / 100);
 
-    printf("Enter a three-digit number: ");
-    scanf("%d", &n);
-
-    printf("The reversal is: %d%d%d\n", n % 10, n / 10 % 10, n / 100);
-
-    return 0;
+  return 0;
 }
