503b489a146ce6e381af6810aae1fea1fee2d014
Sangmin In <84954087+dlstkdals123@users.noreply.github.com>
2022-06-26 18:02:54 +0900

    Create ch06p01.c

---
+++ ch06/projects/01/ch06p01.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	float x, Max = 0.0f;
+	
+	do {
+		printf("Enter a number : ");
+		scanf("%f", &x);
+		if (x > Max) Max = x;
+	} while (x > 0);
+
+	printf("The Largest number entered was %.2f\n", Max);
+
+	return 0;
+}
