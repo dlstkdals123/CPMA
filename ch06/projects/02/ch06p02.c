c8fd85b9d579c2596d5c86632592184c1859ab60
Sangmin In <84954087+dlstkdals123@users.noreply.github.com>
2022-06-26 18:15:45 +0900

    Update ch06p02.c

--- ch06/projects/02/ch06p02.c
+++ ch06/projects/02/ch06p02.c
@@ -8,11 +8,11 @@ int main(void)
 	scanf("%d %d", &i1, &i2);
 
 	while (i2 != 0) {
-		Remainder = i1 % i2;
-		i1 = i2;
-		i2 = Remainder;
-	}
-	printf("Greatest common divisor : %d\n", i1);
-
-	return 0;
+        Remainder = i1 % i2;
+        i1 = i2;
+        i2 = Remainder;
+    }
+    printf("Greatest common divisor : %d\n", i1);
+    
+    return 0;
 }
