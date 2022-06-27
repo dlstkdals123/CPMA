b8067a067553cf8ab8789f91057210075237d91a
Sangmin In <84954087+dlstkdals123@users.noreply.github.com>
2022-06-27 14:33:28 +0900

    Update ch05p06.c

--- ch05/projects/06/ch05p06.c
+++ ch05/projects/06/ch05p06.c
@@ -4,7 +4,7 @@
 
 int main(void)
 {
-	int d, i1, i2, i3, i4, i5, j1, j2, j3, j4, j5, CheckDigit, first_sum, second_sum, total;
+	int d, i1, i2, i3, i4, i5, j1, j2, j3, j4, j5, checkDigit, first_sum, second_sum, total;
 
 	printf("Enter the first (single) digit : ");
 	scanf("%1d", &d);
@@ -13,13 +13,13 @@ int main(void)
 	printf("Enter second group of five digits : ");
 	scanf("%1d%1d%1d%1d%1d", &j1, &j2, &j3, &j4, &j5);
 	printf("Enter the check digit : ");
-	scanf("%d", &CheckDigit);
+	scanf("%d", &checkDigit);
 
 	first_sum = d + i2 + i4 + j1 + j3 + j5;
 	second_sum = i1 + i3 + i5 + j2 + j4;
 	total = 3 * first_sum + second_sum;
 
-	if (CheckDigit == 9 - ((total - 1) % 10)) printf("VALID\n");
+	if (checkDigit == 9 - ((total - 1) % 10)) printf("VALID\n");
 	else printf("NOT VALID\n");
 
 	return 0;
