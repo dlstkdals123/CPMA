1935b8f38a381680bb4a8b06fb8a55b039e5df8c
Sangmin In <84954087+dlstkdals123@users.noreply.github.com>
2022-06-27 14:35:50 +0900

    Update ch05p09.c

--- ch05/projects/09/ch05p09.c
+++ ch05/projects/09/ch05p09.c
@@ -2,18 +2,18 @@
 
 int main(void)
 {
-	int Month1, Day1, Year1,
-		Month2, Day2, Year2;
+	int month1, day1, year1,
+		month2, day2, year2;
 
 	printf("Enter first date (mm/dd/yy) : ");
-	scanf("%d /%d/ %d", &Month1, &Day1, &Year1);
+	scanf("%d /%d/ %d", &month1, &day1, &year1);
 	printf("Enter second date (mm/dd/yy) : ");
-	scanf("%d /%d/ %d", &Month2, &Day2, &Year2);
+	scanf("%d /%d/ %d", &month2, &day2, &year2);
 
-	if (Year1 * 10000 + Month1 * 100 + Day1 < Year2 * 10000 + Month2 * 100 + Day2)
-		printf("%d/%d/%.2d is earlier than %d/%d/%.2d", Month1, Day1, Year1, Month2, Day2, Year2);
+	if (year1 * 10000 + month1 * 100 + day1 < year2 * 10000 + month2 * 100 + day2)
+		printf("%d/%d/%.2d is earlier than %d/%d/%.2d", month1, day1, year1, month2, day2, year2);
 	else
-		printf("%d/%d/%.2d is earlier than %d/%d/%.2d", Month2, Day2, Year2, Month1, Day1, Year1);
+		printf("%d/%d/%.2d is earlier than %d/%d/%.2d", month2, day2, year2, month1, day1, year1);
 
 	return 0;
 
