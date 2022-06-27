71db0919717424dee950a64f141cbf8958144d99
Sangmin In <84954087+dlstkdals123@users.noreply.github.com>
2022-06-27 14:31:50 +0900

    Update ch05p03.c

--- ch05/projects/03/ch05p03.c
+++ ch05/projects/03/ch05p03.c
@@ -4,36 +4,36 @@
 
 int main(void)
 {
-	int Share;
-	float PricePerShare, Value, OriginalCommission, RivalCommission;
+	int share;
+	float pricePerShare, value, originalCommission, rivalCommission;
 
 	printf("Enter number of shares : ");
-	scanf("%f", &Share);
+	scanf("%f", &share);
 	printf("Enter price per share : ");
-	scanf("%f", &PricePerShare);
-	Value = Share * PricePerShare;
+	scanf("%f", &pricePerShare);
+	value = share * pricePerShare;
 
-	if (Value < 2500.00f)
-		OriginalCommission = 30.00f + .017f * Value;
-	else if (Value < 6250.00f)
-		OriginalCommission = 56.00f + .0066f * Value;
-	else if (Value < 20000.00f)
-		OriginalCommission = 76.00f + 0.0034f * Value;
-	else if (Value < 50000.00f)
-		OriginalCommission = 100.00f + 0.0022f * Value;
-	else if (Value < 500000.00f)
-		OriginalCommission = 155.00f + 0.0011f * Value;
+	if (value < 2500.00f)
+		originalCommission = 30.00f + .017f * value;
+	else if (value < 6250.00f)
+		originalCommission = 56.00f + .0066f * value;
+	else if (value < 20000.00f)
+		originalCommission = 76.00f + 0.0034f * value;
+	else if (value < 50000.00f)
+		originalCommission = 100.00f + 0.0022f * value;
+	else if (value < 500000.00f)
+		originalCommission = 155.00f + 0.0011f * value;
 	else
-		OriginalCommission = 255.00f + 0.0009f * Value;
+		originalCommission = 255.00f + 0.0009f * value;
 
-	if (OriginalCommission < 39.00f)
-		OriginalCommission = 39.00f;
+	if (originalCommission < 39.00f)
+		originalCommission = 39.00f;
 
-	if (Share < 2000) RivalCommission = 33.00f + 0.03f * Share;
-	else RivalCommission = 33.00f + 0.02f * Share;
+	if (Share < 2000) rivalCommission = 33.00f + 0.03f * share;
+	else rivalCommission = 33.00f + 0.02f * share;
 
-	printf("Commission charged by the original broker : $%.2f\n", OriginalCommission);
-	printf("Commission charged by the rival broker : $%.2f\n", RivalCommission);
+	printf("Commission charged by the original broker : $%.2f\n", originalCommission);
+	printf("Commission charged by the rival broker : $%.2f\n", rivalCommission);
 
 	return 0;
 }
