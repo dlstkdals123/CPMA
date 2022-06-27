d282182ad3cdceb522de0379b4dd6538e5fdff8f
Sangmin In <84954087+dlstkdals123@users.noreply.github.com>
2022-06-27 19:30:17 +0900

    Update ch07p05.c

--- ch07/projects/05/ch07p05.c
+++ ch07/projects/05/ch07p05.c
@@ -1,39 +1,32 @@
-#include <stdio.h>
 #include <ctype.h>
+#include <stdio.h>
 
-int main(void) {
-	unsigned char ch;
-	short sum = 0;
+int main(void)
+{
+  int sum = 0;
+  char ch;
 
-	printf("Enter a word : ");
-	while ((ch = toupper(getchar())) != '\n') {
-		switch (ch) {
-		case 'A': case 'E': case 'I': case 'L': case 'N': case 'O': case 'R': case 'S': case 'T': case 'U':
-			sum += 1;
-			break;
-		case 'D': case 'G':
-			sum += 2;
-			break;
-		case 'B': case 'C': case 'M': case 'P':
-			sum += 3;
-			break;
-		case 'F': case 'H': case 'V': case 'W': case 'Y':
-			sum += 4;
-			break;
-		case 'K':
-			sum += 5;
-			break;
-		case 'J': case 'X':
-			sum += 8;
-			break;
-		case 'Q': case 'Z':
-			sum += 10;
-			break;
-		default:
-			break;
-		}
-	}
-	printf("Scrabble value : %hd", sum);
+  printf("Enter a word: ");
 
-	return 0;
+  while ((ch = getchar()) != '\n')
+    switch (toupper(ch)) {
+      case 'D': case 'G':
+        sum += 2; break;
+      case 'B': case 'C': case 'M': case 'P':
+        sum += 3; break;
+      case 'F': case 'H': case 'V': case 'W': case 'Y':
+        sum += 4; break;
+      case 'K':
+        sum += 5; break;
+      case 'J': case 'X':
+        sum += 8; break;
+      case 'Q': case 'Z':
+        sum += 10; break;
+      default:
+        sum++; break;
+    }
+
+  printf("Scrabble value: %d\n", sum);
+
+  return 0;
 }
