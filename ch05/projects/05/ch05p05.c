fd29b866082c6bf007c8010d10be2ae1c1ac49b8
Sangmin In <84954087+dlstkdals123@users.noreply.github.com>
2022-06-27 14:32:59 +0900

    Update ch05p05.c

--- ch05/projects/05/ch05p05.c
+++ ch05/projects/05/ch05p05.c
@@ -4,20 +4,20 @@
 
 int main(void)
 {
-	int Income;
-	float Tax;
+	int income;
+	float tax;
 
 	printf("Enter taxable income : ");
-	scanf("%d", &Income);
+	scanf("%d", &income);
 
-	if (Income < 750) Tax = 0.01f * Income;
-	else if (Income <= 2250) Tax = 7.50f + 0.02f * (Income - 750);
-	else if (Income <= 3750) Tax = 37.50f + 0.03f * (Income - 2250);
-	else if (Income <= 5250) Tax = 82.50f + 0.04f * (Income - 3750);
-	else if (Income <= 7000) Tax = 142.50f + 0.05f * (Income - 5250);
-	else Tax = 230.00f + 0.06f * (Income - 7000);
+	if (income < 750) tax = 0.01f * income;
+	else if (income <= 2250) tax = 7.50f + 0.02f * (income - 750);
+	else if (income <= 3750) tax = 37.50f + 0.03f * (income - 2250);
+	else if (income <= 5250) tax = 82.50f + 0.04f * (income - 3750);
+	else if (income <= 7000) tax = 142.50f + 0.05f * (income - 5250);
+	else tax = 230.00f + 0.06f * (income - 7000);
 
-	printf("Tax due : $%.2f\n", Tax);
+	printf("Tax due : $%.2f\n", tax);
 
 	return 0;
 }
